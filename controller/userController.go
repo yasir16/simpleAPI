dd03d4504b3a296baa6068bc470278537822224f
yasir16 <rayas143120@gmail.com>
2021-01-09 00:00:51 +0700

    add api single and create user

--- controller/userController.go
+++ controller/userController.go
@@ -29,3 +29,47 @@ func (idb *InDB) GetUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, result)
 
 }
+
+func (idb *InDB) GetUser(c *gin.Context) {
+	var (
+		person structs.User
+		result gin.H
+	)
+	id := c.Param("id")
+	err := idb.DB.Where("id = ?", id).First(&person).Error
+	if err != nil {
+		result = gin.H{
+			"result": err.Error(),
+			"count":  0,
+		}
+	} else {
+		result = gin.H{
+			"result": person,
+			"count":  1,
+		}
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+func (idb *InDB) CreateUser(c *gin.Context) {
+	var (
+		user   structs.User
+		result gin.H
+	)
+	err := c.Bind(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": "can't bind struct",
+			"error":   err.Error(),
+		})
+	} else {
+		idb.DB.Create(&user)
+		result = gin.H{
+			"result": user,
+		}
+		c.JSON(http.StatusOK, result)
+	}
+
+}
